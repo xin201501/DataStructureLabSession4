bb58a66fbfba617ac98c332e8b4e0b96b9143150
23867 <2386772327@qq.com>
2020-12-26 16:24:03 +0800

    2020/12/26 16:23

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@ int main() {
     system("chcp 65001");
     MapAdjacencyListGraphFactory factory;
     auto cityMap = factory.getBean();
-    std::string startCity, endCity;
     std::cout << "Part 1:testing the BFS function\n";
     std::cout << cityMap.BFS() << '\n';
     std::cout << "Part 2:testing the DFS function:\n";
     std::cout << cityMap.DFS() << '\n';
     std::cout << "Part 3: testing the min cost of every two cities\n";
+    std::string startCity, endCity;
     std::cout << "please input the names of the two cities,double 0 to end:" << std::endl;
     while (std::cin >> startCity >> endCity) {
         if (startCity == "0" && endCity == "0") {
